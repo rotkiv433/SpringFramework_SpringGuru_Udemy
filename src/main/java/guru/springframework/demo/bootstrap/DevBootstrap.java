b4e5afa591980a1873cc0b01a50177d3a9573fa3
Viktor Ilievski <viktor.ilievski@students.finki.ukim.mk>
2020-07-22 01:46:14 +0200

    Added a new Model and repository called Publisher.

--- src/main/java/guru/springframework/demo/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/demo/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package guru.springframework.demo.bootstrap;
 
 import guru.springframework.demo.model.Author;
 import guru.springframework.demo.model.Book;
+import guru.springframework.demo.model.Publisher;
 import guru.springframework.demo.repositories.AuthorRepository;
 import guru.springframework.demo.repositories.BookRepository;
+import guru.springframework.demo.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,28 +15,40 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     private void initData() {
+        Publisher publisher1 = new Publisher("foo", "garbage");
+        Publisher publisher2 = new Publisher("foo2", "trash");
+
+
+
+
+
         //Eric
         Author eric = new Author("Eric", "Evans");
-        Book aaa = new Book("Domain Driven Design", "1234", "Harper Collins");
+
+        Book aaa = new Book("Domain Driven Design", "1234", publisher1);
         eric.getBooks().add(aaa);
         aaa.getAuthors().add(eric);
 
+        publisherRepository.save(publisher1);
         authorRepository.save(eric);
         bookRepository.save(aaa);
 
         //Rod
         Author rod = new Author("Rod", "Johnson");
-        Book bbb = new Book("J2EE Development withour EJB", "234444", "Worx");
+        Book bbb = new Book("J2EE Development withour EJB", "234444",publisher2);
         rod.getBooks().add(bbb);
         bbb.getAuthors().add(rod);
 
+        publisherRepository.save(publisher2);
         authorRepository.save(rod);
         bookRepository.save(bbb);
     }
--- src/main/java/guru/springframework/demo/model/Author.java
+++ src/main/java/guru/springframework/demo/model/Author.java
@@ -10,7 +10,6 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String firstName;
     private String lastName;
 
